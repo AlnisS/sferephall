4616a83356fe923cd5262d088536e582c53b8da2
AlnisS <alnis.smidchens@gmail.com>
2022-04-01 21:08:13 -0700

    Preliminary camera system and aim system

---
+++ main.gd
@@ -0,0 +1,34 @@
+extends Spatial
+
+var last_mouse_position: Vector2 = Vector2.ZERO
+
+func _physics_process(delta):
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	
+	# move camera vertically
+	var ball_pos: Vector3 = $Ball.get_global_transform().origin
+	var camera_pos: Vector3 = $CameraPivot.get_global_transform().origin
+	# move to match position
+	var movement: Vector3 = (ball_pos - camera_pos) * 2
+	# an extra kick to help it "lead" the ball's velocity
+	movement += $Ball.linear_velocity * 0.3
+	$CameraPivot.translate(Vector3(0, movement.y * delta, 0))
+	
+	# rotate camera if right mouse button is dragged
+	if Input.is_action_pressed("rotate"):
+		var angle: float = mouse_position.x - last_mouse_position.x
+		angle *= -0.005
+		$CameraPivot.rotate_y(angle)
+	
+	if Input.is_action_just_pressed("place"):
+		var ray_length = 1000
+		var camera = $CameraPivot/Camera
+		var from = camera.project_ray_origin(mouse_position)
+		var to = from + camera.project_ray_normal(mouse_position) * ray_length
+		var space_state = get_world().get_direct_space_state()
+		var result = space_state.intersect_ray(from, to)
+		if result:
+			$Target.translation = result.position
+	
+	last_mouse_position = mouse_position
+
