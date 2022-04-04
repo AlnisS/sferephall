7053ce896799b240e12dfa3d1a4a1e5fdb0b14d2
AlnisS <alnis.smidchens@gmail.com>
2022-04-03 17:48:37 -0700

    Add pausing

--- score_tracker.gd
+++ score_tracker.gd
@@ -2,3 +2,10 @@ extends Node
 
 var last_time = 0.0
 var best_time = 0.0
+
+func _ready():
+	set_pause_mode(PAUSE_MODE_PROCESS)
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = not get_tree().paused
